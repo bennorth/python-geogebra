71fb8495efa8f0135728f133dad54f99116649e3
Ben North <ben@redfrontdoor.org>
2022-11-10 15:03:19 +0000

    Distance: Implement

--- src/ggb/Distance.js
+++ src/ggb/Distance.js
@@ -1,2 +1,10 @@
 Sk.builtins.Distance = new Sk.builtin.func(function (pyObj1, pyObj2) {
+  // TODO: Check that pyObj1 and pyObj2 are Geogebra objects.
+
+  const cmd = `Distance(${pyObj1.$ggbLabel}, ${pyObj2.$ggbLabel})`;
+  const distObjLabel = ggbApi.evalCommandGetLabels(cmd);
+  const distance = ggbApi.getValue(distObjLabel);
+  ggbApi.deleteObject(distObjLabel);
+
+  return new Sk.builtin.float_(distance);
 });
