7429b04c4da4b529e8d7ca13dd16d20d4194d9cc
Ben North <ben@redfrontdoor.org>
2022-11-10 15:01:53 +0000

    Line: Add ctor, __new__

--- src/ggb/Line.js
+++ src/ggb/Line.js
@@ -1,2 +1,15 @@
 Sk.builtins.Line = Sk.abstr.buildNativeClass("Line", {
+  constructor: function Point(pyObj1, pyObj2) {
+    const ggbLabel1 = pyObj1.$ggbLabel;
+    const ggbLabel2 = pyObj2.$ggbLabel;
+    const cmd = `Line(${ggbLabel1}, ${ggbLabel2})`;
+    this.$ggbLabel = ggbApi.evalCommandGetLabels(cmd);
+  },
+  slots: {
+    tp$new(args, kwargs) {
+      Sk.abstr.checkNoKwargs("Line", kwargs);
+      Sk.abstr.checkArgsLen("Line", args, 2, 2);
+      return new Sk.builtins.Line(...args);
+    },
+  },
 });
