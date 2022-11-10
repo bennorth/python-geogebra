2ceddaa1d4f0a8965f0c514f31b2bcf306ffd252
Ben North <ben@redfrontdoor.org>
2022-11-10 14:56:56 +0000

    Point: Define ctor, __new__

--- src/ggb/Point.js
+++ src/ggb/Point.js
@@ -1,2 +1,18 @@
 Sk.builtins.Point = Sk.abstr.buildNativeClass("Point", {
+  constructor: function Point(pyX, pyY) {
+    const x = Sk.ffi.remapToJs(pyX);
+    const y = Sk.ffi.remapToJs(pyY);
+
+    const cmd = `(${x}, ${y})`;
+    const lbl = ggbApi.evalCommandGetLabels(cmd);
+
+    this.$ggbLabel = lbl;
+  },
+  slots: {
+    tp$new(args, kwargs) {
+      Sk.abstr.checkNoKwargs("Point", kwargs);
+      Sk.abstr.checkArgsLen("Point", args, 2, 2);
+      return new Sk.builtins.Point(...args);
+    },
+  },
 });
