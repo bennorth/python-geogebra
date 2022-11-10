df40ec524ff6c11d39cc94f7a48b4365515b7920
Ben North <ben@redfrontdoor.org>
2022-11-10 15:02:05 +0000

    Line: Add basic input validation

--- src/ggb/Line.js
+++ src/ggb/Line.js
@@ -2,6 +2,10 @@ Sk.builtins.Line = Sk.abstr.buildNativeClass("Line", {
   constructor: function Point(pyObj1, pyObj2) {
     const ggbLabel1 = pyObj1.$ggbLabel;
     const ggbLabel2 = pyObj2.$ggbLabel;
+
+    if (ggbLabel1 == null || ggbLabel2 == null)
+      throw new Sk.builtin.TypeError("not GGB things");
+
     const cmd = `Line(${ggbLabel1}, ${ggbLabel2})`;
     this.$ggbLabel = ggbApi.evalCommandGetLabels(cmd);
   },
