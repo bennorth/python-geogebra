a19034e09bad82001b15ce4f57e14e4b7d685581
Ben North <ben@redfrontdoor.org>
2022-11-11 10:49:59 +0000

    First build

---
+++ dist/ggb/Line.js
@@ -0,0 +1,19 @@
+Sk.builtins.Line = Sk.abstr.buildNativeClass("Line", {
+  constructor: function Point(pyObj1, pyObj2) {
+    const ggbLabel1 = pyObj1.$ggbLabel;
+    const ggbLabel2 = pyObj2.$ggbLabel;
+
+    if (ggbLabel1 == null || ggbLabel2 == null)
+      throw new Sk.builtin.TypeError("not GGB things");
+
+    const cmd = `Line(${ggbLabel1}, ${ggbLabel2})`;
+    this.$ggbLabel = ggbApi.evalCommandGetLabels(cmd);
+  },
+  slots: {
+    tp$new(args, kwargs) {
+      Sk.abstr.checkNoKwargs("Line", kwargs);
+      Sk.abstr.checkArgsLen("Line", args, 2, 2);
+      return new Sk.builtins.Line(...args);
+    },
+  },
+});
---
+++ dist/ggb/Point.js
@@ -0,0 +1,55 @@
+Sk.builtins.Point = Sk.abstr.buildNativeClass("Point", {
+  constructor: function Point(pyX, pyY) {
+    const x = Sk.ffi.remapToJs(pyX);
+    const y = Sk.ffi.remapToJs(pyY);
+
+    const cmd = `(${x}, ${y})`;
+    const lbl = ggbApi.evalCommandGetLabels(cmd);
+
+    this.$ggbLabel = lbl;
+
+    this.$updateHandlers = [];
+    ggbApi.registerObjectUpdateListener(lbl, () => this.$fireUpdateEvents());
+  },
+  slots: {
+    tp$new(args, kwargs) {
+      Sk.abstr.checkNoKwargs("Point", kwargs);
+      Sk.abstr.checkArgsLen("Point", args, 2, 2);
+      return new Sk.builtins.Point(...args);
+    },
+  },
+  proto: {
+    $xCoord() {
+      return ggbApi.getXcoord(this.$ggbLabel);
+    },
+    $yCoord() {
+      return ggbApi.getYcoord(this.$ggbLabel);
+    },
+    $fireUpdateEvents() {
+      this.$updateHandlers.forEach((fun) => {
+        Sk.misceval.callsimOrSuspend(fun);
+      });
+    },
+  },
+  methods: {
+    when_moved: {
+      $meth(pyFun) {
+        this.$updateHandlers.push(pyFun);
+        return pyFun;
+      },
+      $flags: { OneArg: true },
+    },
+  },
+  getsets: {
+    x: {
+      $get() {
+        return new Sk.builtin.float_(this.$xCoord());
+      },
+    },
+    y: {
+      $get() {
+        return new Sk.builtin.float_(this.$yCoord());
+      },
+    },
+  },
+});
