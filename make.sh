8d21d5411e7e3ea792f93de41ae015dea51618d4
Ben North <ben@redfrontdoor.org>
2022-11-11 10:54:41 +0000

    Build to "docs" to allow GitHub pages

--- make.sh
+++ make.sh
@@ -5,12 +5,12 @@ cd_or_fail() { cd "$1" || exit 1; }
 REPO_ROOT=$(cd_or_fail "$(dirname "$0")"; pwd -P)
 
 cd_or_fail "$REPO_ROOT"
-mkdir -p dist
+mkdir -p docs
 
 (
     cd_or_fail src
     git ls-files . -z | tar --file - --create --null --files-from -
 ) | (
-    cd_or_fail dist
+    cd_or_fail docs
     tar --file - --extract --verbose
 )
