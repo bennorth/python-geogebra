e6653cf28dbfa11eeaf67fdc46b32d7ef8b9df32
Michael Borcherds <github@borcherds.co.uk>
2022-11-12 15:04:44 +0000

    Better error; tidying

--- src/ggb/FitPoly.js
+++ src/ggb/FitPoly.js
@@ -1,32 +1,27 @@
 Sk.builtins.FitPoly = Sk.abstr.buildNativeClass("FitPoly", {
   constructor: function FitPoly(pyList, pyObj) {
 	  
-	  
-	  window.pyList = pyList;
+	window.pyList = pyList;
 	    
-		// TODO: check why remapToJs() isn't working 
-		// and remove hack
-		//var jsList = Sk.ffi.remapToJs(pyList);
-		var jsList = pyList.v;
+	// TODO: check why remapToJs() isn't working 
+	// and remove hack
+	//var jsList = Sk.ffi.remapToJs(pyList);
+	var jsList = pyList.v;
 	  
 	// check first for GGB object, then assume numbers
     const jsNumOrLabel = pyObj.$ggbLabel || Math.round(Sk.ffi.remapToJs(pyObj));
-	  oldconsole("jsList.length = " + jsList.length);
- var ggbList = "{";
-  for (var i = 0; i < jsList.length; i++) {
-	  oldconsole(jsList[i]);
-    if (ggbApi.getObjectType(jsList[i].$ggbLabel) != GGB_POINT) {
-      throw jsList[i] + " is not a point";
-    }
 
-    ggbList += jsList[i].$ggbLabel + ",";
-  }
-  // replace last , with }
-  ggbList = ggbList.replace(/.$/, "}");
+    var ggbList = "{";
+    for (var i = 0; i < jsList.length; i++) {
+      if (ggbApi.getObjectType(jsList[i].$ggbLabel) != GGB_POINT) {
+        throw jsList[i].$ggbLabel + " = " + jsList[i] + " is not a point";
+      }
+      ggbList += jsList[i].$ggbLabel + ",";
+    }
+    // replace last , with }
+    ggbList = ggbList.replace(/.$/, "}");
   
-	  
-	  // "FitPoly(" + ggbList + "," + jsNum + ")"
-	    const cmd = `FitPoly(${ggbList}, ${jsNumOrLabel})`;
+    const cmd = `FitPoly(${ggbList}, ${jsNumOrLabel})`;
 	
     this.$ggbLabel = ggbApi.evalCommandGetLabels(cmd);
 
